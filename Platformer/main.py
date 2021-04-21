2a9b21b3ddd8e141e6cda715513b4db2951afcd7
JLew15 <zombears@icloud.com>
2021-04-20 22:37:47 -0600

    DJKLSJFKLS

--- Platformer/main.py
+++ Platformer/main.py
@@ -12,6 +12,7 @@ class Game:
         pg.display.set_caption("Jumpy Jump")
         self.clock = pg.time.Clock()
         self.running = True
+        self.fontName = pg.font.match_font(FONT_NAME)
 
     def run(self):
         self.playing = True
@@ -22,24 +23,45 @@ class Game:
             self.draw()
 
     def new(self):
+        self.score = 0
         self.allSprites = pg.sprite.Group()
         self.platforms = pg.sprite.Group()
-        self.player = Player()
+        self.player = Player(self)
         self.allSprites.add(self.player)
-        p1 = Platform(0, HEIGHT - 40, WIDTH, 40)
-        self.allSprites.add(p1)
-        self.platforms.add(p1)
-        p2 = Platform(WIDTH/2 - 50, HEIGHT * 3 / 4, 100, 20)
-        self.allSprites.add(p2)
-        self.platforms.add(p2)
+        for plat in PLATFORM_LIST:
+            p = Platform(*plat)
+            self.allSprites.add(p)
+            self.platforms.add(p)
         self.run()
 
     def update(self):
         self.allSprites.update()
-        hits = pg.sprite.spritecollide(self.player, self.platforms, False)
-        if hits:
-            self.player.pos.y = hits[0].rect.top
-            self.player.vel.y = 0
+        if self.player.vel.y > 0:
+            hits = pg.sprite.spritecollide(self.player, self.platforms, False)
+            if hits:
+                self.player.pos.y = hits[0].rect.top
+                self.player.vel.y = 0
+        if self.player.rect.top <= HEIGHT /4:
+            self.player.pos.y += abs(self.player.vel.y)
+            for plat in self.platforms:
+                plat.rect.y += abs(self.player.vel.y)
+                if plat.rect.top >= HEIGHT:
+                    plat.kill()
+                    self.score += 10
+
+        if self.player.rect.bottom > HEIGHT:
+            for sprite in self.allSprites:
+                sprite.rect.y -= max(self.player.vel.y, 10)
+                if sprite.rect.bottom < 0:
+                    sprite.kill()
+        if len(self.platforms) == 0:
+            self.playing = False
+
+        while len(self.platforms) < 6:
+            width = random.randrange(50, 100)
+            p = Platform(random.randrange(0, WIDTH-width), random.randrange(-75, -30), width, 20)
+            self.platforms.add(p)
+            self.allSprites.add(p)
 
     def events(self):
         for event in pg.event.get():
@@ -47,10 +69,14 @@ class Game:
                 if self.playing:
                     self.playing = False
                 self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
 
     def draw(self):
         self.screen.fill(BLACK)
         self.allSprites.draw(g.screen)
+        self.drawText(str(self.score), 22, WHITE, WIDTH/2, 15)
         pg.display.flip()
 
     def showStartScreen(self):
@@ -58,6 +84,13 @@ class Game:
 
     def showGOScreen(self):
         pass
+
+    def drawText(self, text, size, color, x, y):
+        font = pg.font.Font(self.fontName, size)
+        textSurface = font.render(text, True, color)
+        textRect = textSurface.get_rect()
+        textRect.midtop = (x, y)
+        self.screen.blit(textSurface, textRect)
 
 
 g = Game()
--- Platformer/sprites.py
+++ Platformer/sprites.py
@@ -5,8 +5,9 @@ vec = pg.math.Vector2
 
 
 class Player(pg.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, game):
         super(Player, self).__init__()
+        self.game = game
         self.image = pg.Surface((30, 40))
         self.image.fill(YELLOW)
         self.rect = self.image.get_rect()
@@ -15,8 +16,15 @@ class Player(pg.sprite.Sprite):
         self.vel = vec(0, 0)
         self.acc = vec(0, 0)
 
+    def jump(self):
+        self.rect.x += 1
+        hits = pg.sprite.spritecollide(self, self.game.platforms, False)
+        self.rect.x -= 1
+        if hits:
+            self.vel.y = -PLAYER_JUMP
+
     def update(self):
-        self.acc = vec(0, 0.5)
+        self.acc = vec(0, PLAYER_GRAV)
         keys = pg.key.get_pressed()
         if keys[pg.K_LEFT]:
             self.acc.x = -PLAYER_ACC
@@ -26,9 +34,9 @@ class Player(pg.sprite.Sprite):
         self.acc.x += self.vel.x * PLAYER_FRICTION
         self.vel += self.acc
         self.pos += self.vel + 0.5 * self.acc
-        if self.pos.x > WIDTH:
+        if self.pos.x >= WIDTH:
             self.pos.x = 0
-        if self.pos.x < 0:
+        if self.pos.x <= 0:
             self.pos.x = WIDTH
         self.rect.midbottom = self.pos
 
